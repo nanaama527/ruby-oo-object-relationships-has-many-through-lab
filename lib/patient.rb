e8b64dfac495ff14c1747356f16af647db0d38a1
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-02 23:22:19 -0500

    commit tree 7221e9b24fa7234ec1bb7871adec35fac9e6fbef

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,4 +7,21 @@ class Patient
         @name = name
         @@all << self 
     end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        appt = Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|appt| appt.patient == self}
+    end
+
+    def doctors
+        self.appointments.collect {|appt| appt.doctor}
+    end
+
 end
